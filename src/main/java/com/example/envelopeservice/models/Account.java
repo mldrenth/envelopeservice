ce7c0991d46320c43805d6549c3e77b7aaf4854a
Marijke Drenth <drenth.marijke@googlemail.com>
2022-06-16 18:59:00 +0300

    Add functionality to move money from main account to envelope]

--- src/main/java/com/example/envelopeservice/models/Account.java
+++ src/main/java/com/example/envelopeservice/models/Account.java
@@ -1,5 +1,6 @@
 package com.example.envelopeservice.models;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class Account {
@@ -7,11 +8,13 @@ public class Account {
     private String name;
     private AccountType accountType;
     private ArrayList<Envelope> envelopes;
+    private BigDecimal availableAmount;
 
     public Account(String name, AccountType accountType) {
         this.name = name;
         this.accountType = accountType;
         this.envelopes = new ArrayList<>();
+        this.availableAmount = new BigDecimal("0.00");
     }
 
     public String getName() {
@@ -42,7 +45,40 @@ public class Account {
         return this.envelopes.size();
     }
 
+    public BigDecimal getAvailableAmount() {
+        return availableAmount;
+    }
+
+    public void setAvailableAmount(BigDecimal availableAmount) {
+        this.availableAmount = availableAmount;
+    }
+
+    public void deposit(BigDecimal depositAmount) {
+        this.setAvailableAmount(this.availableAmount.add(depositAmount));
+    }
+
+    public BigDecimal withdraw(BigDecimal withdrawAmount) {
+        if (this.canWithdrawAmount(withdrawAmount)) {
+            this.setAvailableAmount(this.availableAmount.subtract(withdrawAmount));
+            return withdrawAmount;
+        }
+        else {
+            BigDecimal newWithdrawAmount = this.availableAmount;
+            this.setAvailableAmount(new BigDecimal("0.00"));
+            return newWithdrawAmount;
+        }
+    }
+
+    public Boolean canWithdrawAmount(BigDecimal withdrawAmount) {
+        return this.availableAmount.compareTo(withdrawAmount) >= 0;
+    }
+
     public void addEnvelope(Envelope envelope) {
         this.envelopes.add(envelope);
+    }
+
+    public void depositIntoEnvelope(BigDecimal depositAmount, int index) {
+        BigDecimal withdrawnAmount = this.withdraw(depositAmount);
+        this.getEnvelopes().get(index).deposit(withdrawnAmount);
     }
 }
--- src/test/java/com/example/envelopeservice/AccountTest.java
+++ src/test/java/com/example/envelopeservice/AccountTest.java
@@ -6,6 +6,8 @@ import com.example.envelopeservice.models.Envelope;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.Assert.assertEquals;
 
 public class AccountTest {
@@ -41,6 +43,42 @@ public class AccountTest {
         myAccount.addEnvelope(travel);
         myAccount.addEnvelope(groceries);
         assertEquals(2, myAccount.getEnvelopeAmount());
+    }
+
+    @Test
+    public void hasAvailableAmount0() {
+        assertEquals(new BigDecimal("0.00"), myAccount.getAvailableAmount());
+    }
+
+    @Test
+    public void canDepositIntoAvailableAmount() {
+        myAccount.deposit(new BigDecimal("600.00"));
+        assertEquals(new BigDecimal("600.00"), myAccount.getAvailableAmount());
+    }
+
+    @Test
+    public void canWithdrawMoney() {
+        myAccount.deposit(new BigDecimal("200"));
+        myAccount.withdraw(new BigDecimal("100"));
+        assertEquals(new BigDecimal("100.00"), myAccount.getAvailableAmount());
+
+    }
+
+    @Test
+    public void canNotWithdrawNegativeAmount() {
+        myAccount.deposit(new BigDecimal("200"));
+        BigDecimal withdrawnAmount = myAccount.withdraw(new BigDecimal("250"));
+        assertEquals(new BigDecimal("0.00"), this.myAccount.getAvailableAmount());
+        assertEquals(new BigDecimal("200.00"), withdrawnAmount);
+    }
+
+    @Test
+    public void canAddToEnvelope() {
+        myAccount.addEnvelope(travel);
+        myAccount.deposit(new BigDecimal("200.00"));
+        myAccount.depositIntoEnvelope(new BigDecimal("100.00"), 0);
+        assertEquals(new BigDecimal("100.00"), myAccount.getAvailableAmount());
+        assertEquals(new BigDecimal("100.00"), myAccount.getEnvelopes().get(0).getAmount());
     }
 
 }
